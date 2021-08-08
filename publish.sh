2ee3f496222180567ff0164f76ebfd2ed6f6b83a
hak33m16 <hak33m16@users.noreply.github.com>
2021-08-08 11:41:50 -0400

    Add basic publish script for server

--- publish.sh
+++ publish.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+. ~/.profile
+
 npm run clean
 npm run build
 
-rm -rf /var/www/laddergame.io/html/*
-cp -r src/* /var/www/laddergame.io/html/
+if [[ -z "$PR_NUMBER" ]]; then
+    echo "PR number wasn't set, releasing..."
+    rm -rf /var/www/laddergame.io/html/*
+    cp -r src/* /var/www/laddergame.io/html/
+else
+    echo "PR number was set, performing dev release for PR: $PR_NUMBER..."
+    mkdir -p /var/www/dev.laddergame.io/html/pull-requests/"$PR_NUMBER"
+    rm -rf /var/www/dev.laddergame.io/html/pull-requests/"$PR_NUMBER"/*
+    cp -r src/* /var/www/dev.laddergame.io/html/pull-requests/"$PR_NUMBER"
+fi
