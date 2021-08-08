0271b987850acd8735cbaab259b23deb2842c7cd
hak33m16 <hak33m16@users.noreply.github.com>
2021-08-08 13:13:00 -0400

    Update workflow and publish script to support non-PR builds

--- publish.sh
+++ publish.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+set -e
+
 . ~/.profile
 
+set -x
+
+npm install
 npm run clean
 npm run build
 
-if [[ -z "$PR_NUMBER" ]]; then
-    echo "PR number wasn't set, releasing..."
+if [[ "$BRANCH_NAME" == "master" ]]; then
+    echo "Branch name is 'master', releasing..."
     rm -rf /var/www/laddergame.io/html/*
     cp -r src/* /var/www/laddergame.io/html/
-else
+elif [[ -n "$PR_NUMBER" ]]; then
     echo "PR number was set, performing dev release for PR: $PR_NUMBER..."
     mkdir -p /var/www/dev.laddergame.io/html/pull-requests/"$PR_NUMBER"
     rm -rf /var/www/dev.laddergame.io/html/pull-requests/"$PR_NUMBER"/*
